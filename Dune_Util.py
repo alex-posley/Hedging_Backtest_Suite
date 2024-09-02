dd4b02c1179015c771ebf36f04d4571be7611823
alex-posley <alex.tang@posley.capital>
2024-09-02 10:52:36 +0800

    Dune Data

--- Dune_Util.py
+++ Dune_Util.py
@@ -3,5 +3,6 @@ from dune_client.client import DuneClient
 class DuneData:
     def __init__(self, api_key):
         self.dune = DuneClient(api_key)
-        
-    query_result = dune.get_latest_result(3379698)
+
+    def getResultByQueryId(self, query_id):
+        return self.dune.get_latest_result(query_id)
